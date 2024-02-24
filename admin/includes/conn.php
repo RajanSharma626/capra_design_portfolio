1961f56f127b5a7a54d22a6d4116bb7fc5743bb6
Rajan Sharma <rajankumarsharma681@gmail.com>
2024-02-24 22:51:35 +0530

    add admin panel

---
+++ admin/includes/conn.php
@@ -0,0 +1,15 @@
+<?php
+   session_start();
+   $servername = "localhost";
+   $username   = "root";
+   $password   = "";
+   $dbname     = "capra_design";
+
+   $conn = new mysqli($servername,$username,$password,$dbname);
+//    define('SITE_PATH','http://localhost/animeverse/');
+//    if(!$conn){
+//     die("Connection failed :".mysqli_connect_error());
+//    }else{
+//     echo "Connected Successfully";
+//    }
+?>
